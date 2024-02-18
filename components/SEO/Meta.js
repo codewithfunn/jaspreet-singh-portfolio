00d6fc63ccef1ae8755c289cacc0c13b6dff14b1
jaspreet_suri <60849699+codewithfunn@users.noreply.github.com>
2024-02-19 04:53:12 +0530

    first commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Jaspreet Singh Portfolio - Computer Engineering Student</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Jaspreet Singh Portfolio - Computer Engineering Student" />
+            <meta name="description"
+                content="Jaspreet Singh's  Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Jaspreet Singh" />
+            <meta name="keywords"
+                content="codewithfunn, codewithfunn's portfolio, codewithfunn portfolio, linux, ubuntu portfolio, Jaspreet Singh computer, Jaspreet Singh, Jaspreet ubuntu, Jaspreet Singh ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/favicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Jaspreet Singh Portfolio - Computer Engineering Student" />
+            <meta itemProp="description"
+                content="Jaspreet Singh's (codewithfunn) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/favicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Jaspreet Singh Portfolio - Computer Engineering Student" />
+            <meta name="twitter:description"
+                content="Jaspreet Singh's (codewithfunn) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="jaspreetSingh" />
+            <meta name="twitter:creator" content="jaspreetSingh" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Jaspreet singh Portfolio - Computer Engineering Student" />
+            <meta name="og:description"
+                content="Jaspreet singh's (codewithfunn) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="https://github.com/codewithfunn" />
+            <meta name="og:site_name" content="Jaspreet Singh Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/favicon.ico" />
+            <link rel="apple-touch-icon" href="images/logos/favicon.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
